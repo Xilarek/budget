e4718499ce9c8bb799cf1d0430fbd7ab498c3f77
Dmitry Mikhaylov <dmikhaylov7@gmail.com>
2020-01-24 19:13:48 +0300

    Вторая домашка

---
+++ script/script.js
@@ -0,0 +1,20 @@
+//Объявление переменных для дальнейшей работы 
+//Первый пункт основго задания
+const money = 25000,
+    income = 'фриланс',
+    addExpenses = 'интернет, коммуналка, корм для котов, еда',
+    deposit = true,
+    mission = 50000,
+    period = 6,
+    budgetDay = money / 30;
+//Второй пункт основго задания
+console.log(typeof money );
+console.log(typeof income );
+console.log(typeof addExpenses );
+console.log(addExpenses.length );
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать '+ mission + 'рублей' );
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(','));
+console.log(budgetDay);
+

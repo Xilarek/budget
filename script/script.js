bf3db9790d417d1af09b7580e64cafbbc95c603d
Dmitry Mikhaylov <dmikhaylov7@gmail.com>
2020-01-27 22:45:58 +0300

    Правки домашки

--- script/script.js
+++ script/script.js
@@ -1,58 +1,44 @@
 'use strict';
 //Объявление переменных для дальнейшей работы 
 //Первый пункт основго задания
-let money = 25000,
+let money = +prompt('Ваш месячный доход ?'),
     income = 'фриланс',
-    addExpenses = 'интернет, коммуналка, корм для котов, еда',
-    deposit = true,
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит ?'),
     mission = 50000,
     period = 6,
-    budgetDay = money / 30,
-    expenses1,
-    expenses2,
-    amout1,
-    amout2,
-    budgetMounth;
-//Второй пункт основго задания
-/*console.log(typeof money );
-console.log(typeof income );
-console.log(typeof addExpenses );
-console.log(addExpenses.length );
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать '+ mission + 'рублей' );
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(','));
-console.log(budgetDay);*/
+    expenses1 = prompt('Введите обязательную статью расходов'),
+    expenses2 = prompt('Введите обязательную статью расходов'),
+    amout1 = +prompt('Во сколько это обойдется ?'),
+    amout2 = +prompt('Во сколько это обойдется ?'),
+    budgetDay = money - (amout1 + amout2) / 30,
+    budgetMounth = money - (amout1 - amout2 );
 
-money = +prompt('Ваш месячный доход ?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 addExpenses = addExpenses.split(',');
-deposit = confirm('Есть ли у вас депозит ?');
-expenses1 = prompt('Введите обязательную статью расходов');
-expenses2 = prompt('Введите обязательную статью расходов');
-amout1 = +prompt('Во сколько это обойдется ?');
-amout2 = +prompt('Во сколько это обойдется ?');
-console.log(addExpenses);
-budgetDay = (money - (amout1 + amout2)) / 30;
-budgetDay = Math.floor(budgetDay);
-budgetMounth = (budgetDay * 30);
-console.log('Бюджет на месяц '+ budgetMounth); 
-mission = mission / budgetMounth;
-budgetDay = budgetMounth / 30;
-budgetDay = Math.floor(budgetDay);
-mission = Math.ceil(mission);
-console.log('Цель будет достигнута ' + mission);
-console.log('Бюджет на день ' + budgetDay);
+
+budgetDay = Math.floor(budgetMounth / 30);
+
+const missionMonth = Math.ceil(mission / budgetMounth);
 
 if (budgetDay >= 1200) {
     alert('У вас высокий уровень дохода!');
 } if (budgetDay >= 600 && budgetDay < 1200  ) {
     alert('У вас средний уровень дохода');
-} if (budgetDay < 600) {
+} if (budgetDay < 600 && budgetDay > 0 ) {
     alert('К сожалению у вас уровень дохода ниже среднего');
 } if (budgetDay < 0) {
     alert('Что-то пошло не так');
 }
 
-
+console.log(typeof money );
+console.log(typeof income );
+console.log(typeof addExpenses );
+console.log(addExpenses.length );
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать '+ mission + 'рублей' );
+console.log(budgetDay);
+console.log(addExpenses);
+console.log('Бюджет на месяц '+ budgetMounth); 
+console.log('Цель будет достигнута ' + mission);
+console.log('Бюджет на день ' + budgetDay);
 
